e520774c11c8177f455804e263713229909853c0
Khetan <rkhetan@paypal.com>
2016-04-26 12:50:35 +0530

    basic polymer with data binding

---
+++ index.js
@@ -0,0 +1,16 @@
+<link rel="import" href="./bower_components/polymer/polymer.html">
+<script>
+
+<dom-module id="proto-element">
+
+	<template>
+	<p> This is my local DOM. </p>
+	</template>
+
+	<script>
+	  // register a new element called proto-element
+	  Polymer({
+	    is: "proto-element"
+	  });
+	</script>
+</dom-module>
